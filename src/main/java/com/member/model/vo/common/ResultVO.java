018887418d1a82c6b28744dd4a0d52ce4ac3c079
fengkidding <56989531+fengkidding@users.noreply.github.com>
2019-11-28 15:43:08 +0800

    token数据vo

--- src/main/java/com/member/model/vo/common/ResultVO.java
+++ src/main/java/com/member/model/vo/common/ResultVO.java
@@ -1,16 +1,22 @@
 package com.member.model.vo.common;
 
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * 结果
  *
  * @author f
  * @date 2018-04-23
  */
+@ApiModel(value = "结果vo")
 public class ResultVO<T> {
 
+    @ApiModelProperty(value = "状态码：200成功，-1系统错误，400校验失败")
     private Integer code;
 
+    @ApiModelProperty(value = "信息")
     private String msg;
 
     private T data;
