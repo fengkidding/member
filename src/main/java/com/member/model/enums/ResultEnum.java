a62f56aee321464f69c1615c17b00eda72bad9ed
fengchangteng <1236asdF>
2020-11-19 18:23:10 +0800

    springboot版本升级

--- src/main/java/com/member/model/enums/ResultEnum.java
+++ src/main/java/com/member/model/enums/ResultEnum.java
@@ -10,7 +10,7 @@ public enum ResultEnum {
     /**
      * 成功
      */
-    SUCCESS(200, "成功！"),
+    SUCCESS(0, "成功！"),
     /**
      * 服务器错误
      */
@@ -18,27 +18,55 @@ public enum ResultEnum {
     /**
      * 没有相关数据
      */
-    NO_DATA(401, "数据不存在！"),
+    NO_DATA(10001, "数据不存在！"),
     /**
      * token验证失败，请尝试重新登录
      */
-    NO_TOKEN(402, "token验证失败，请尝试重新登录！"),
+    NO_TOKEN(10002, "token验证失败，请尝试重新登录！"),
     /**
      * redis key null
      */
-    KEY_NONE(403, "key 不能为空！"),
+    KEY_NONE(10003, "key 不能为空！"),
     /**
      * 熔断
      */
-    FALL_BACK(404, "服务异常，触发熔断！"),
+    FALL_BACK(10004, "服务异常，触发熔断！"),
     /**
      * 用户名或密码不正确！
      */
-    MEMBER_NAME_PASSWORD_ERROR(405, "用户名或密码不正确！"),
+    MEMBER_NAME_PASSWORD_ERROR(10005, "用户名或密码不正确！"),
     /**
      * 用户未登陆！
      */
-    MEMBER_LOGIN_ERROR(406, "用户未登陆！"),
+    MEMBER_LOGIN_ERROR(10006, "用户未登陆！"),
+    /**
+     * 扣除用户余额失败
+     */
+    DEDUCT_REMAININGSUM_ERROR(10007, "扣除用户余额失败！"),
+    /**
+     * 产品不存在
+     */
+    PRODUCT_NONE_ERROR(10008, "产品不存在！"),
+    /**
+     * 订单不存在
+     */
+    ORDER_NONE_ERROR(10009, "订单不存在！"),
+    /**
+     * 券码不存在
+     */
+    COUPON_NONE_ERROR(10010, "券码不存在！"),
+    /**
+     * 券码不可用
+     */
+    COUPON_UNUSE_ERROR(10011, "券码不可用！"),
+    /**
+     * 操作超时
+     */
+    LOCK_TIME_OUT_ERROR(10012, "操作超时！"),
+    /**
+     * 适配器参数错误
+     */
+    ADAPTER_PARAM_ERROR(10013, "适配器参数错误！"),
     /**
      * 校验失败
      */
